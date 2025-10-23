fb6701804aaa298fec16350f778dec2090604547
thecodingdilettante <forbesm@usf.edu>
2025-10-23 13:14:50 -0400

    Added two plain buttons

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Counter from "./Counter";
+
+export default function Home() {
+    return (
+        <main>
+            <h1>Sales Dashboard</h1>
+            <Counter/>
+        </main>
+    );
+}
