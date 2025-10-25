596fec0ddaccb407ba70b8eb4719559fcf570044
thecodingdilettante <forbesm@usf.edu>
2025-10-25 12:41:37 -0400

    Added second counter with custom props

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Counter from "../components/Counter.js";
+import CustomCounter from "../components/CustomCounter.js";
 
 export default function Home() {
     return (
@@ -7,7 +8,7 @@ export default function Home() {
             <br/>
             <Counter/>
             <br/>
-            
+            <CustomCounter/>
         
         </main>
     );
--- components/CustomCounter.js
+++ components/CustomCounter.js
@@ -1,9 +1,9 @@
 "use client";
 import {useState} from "react";
 
-export default function Counter({initialCount = 0, initialStep = 1}){
+export default function Counter({initialCount = 2, initialStep = 5}){
     const [count, setCount] = useState(initialCount);
-    const [step, setStep] = useState(1);
+    const [step, setStep] = useState(5);
 
 
     return (
