9bc91f48882bd55ed6bb70cd444a40e0d917bee1
thecodingdilettante <forbesm@usf.edu>
2025-11-18 08:19:25 -0500

    Updated layout slightly

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,10 @@ export default function Home() {
     return (
         <main>
             <h1>Sales Dashboard</h1>
-            <br/>
-            THIS COUNTER STARTS AT 0.
+            <h3>This counter starts at 0.</h3>
             <Counter/>
             <br/>
-            THIS COUNTER STARTS AT 2.
+            <h3>This counter starts at 2.</h3>
             <CustomCounter/>
         
         </main>
